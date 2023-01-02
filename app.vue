e00cbbeed292c1c1a1132cb0ad8583df509450ca
hemo <787615673@qq.com>
2023-01-02 14:53:45 +0800

    feat: 清除所有默认样式

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <NuxtPage />
+    <div>jkjkj</div>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#717EF1",
+          "secondary": "#FF6262",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  //...
+}  
