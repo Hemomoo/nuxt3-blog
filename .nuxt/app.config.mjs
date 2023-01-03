7e342a4b32c9257bdb355ecc6acd3a9d14923d60
邓亮 <liang.deng@fujfu.com>
2023-01-03 09:57:00 +0800

    del  del .nuxt

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/hemooo/Documents/code/nuxt3-blog/node_modules/_defu@6.1.1@defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/hemooo/Documents/code/nuxt3-blog/node_modules/_@nuxt_vite-builder@3.0.0@@nuxt/vite-builder/dist/runtime/vite-node.mjs"
