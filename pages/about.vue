226b4f963a1221d1f872acef0c0a4c78c17f90eb
邓亮 <787615673@qq.com>
2023-02-20 11:39:17 +0800

    修改了名言源

--- pages/about.vue
+++ pages/about.vue
@@ -3,22 +3,23 @@
         <div class=""></div>
         <div class="hero-content text-center  text-white">
             <div class="max-w-md">
-                <h1 class="mb-5 text-5xl font-bold">{{ data.data.tag }}</h1>
-                <p class="mb-5">{{ data.data.content }}</p>
-                <p class="text-right">{{ data.data.origin }}</p>
+                <h1 class="mb-5 text-2xl font-bold">{{ data.from }}</h1>
+                <p class="mb-5">{{ data.hitokoto }}</p>
+                <p class="text-right">{{ data.from_who }}</p>
             </div>
         </div>
     </div>
 </template>
 
-<script setup>
+<script  setup>
 useHead({
     title: '关于我',
     meta: [
         { name: 'description', content: 'My amazing site.' }
     ],
 })
-const { data } = await useFetch('https://api.xygeng.cn/one')
+
+const { data } = await useFetch("https://v1.hitokoto.cn/") 
 </script>
 
 <style scoped>
